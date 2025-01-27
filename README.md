a7e5daea2efd344f2320d841d2c86adf26fa9ed8
nothing <alexandermatthew25@gmail.com>
2025-01-27 14:11:19 +0000

    hopefully it worked

--- README.md
+++ README.md
@@ -28,7 +28,3 @@
 ---
 
 -   :thought_balloon: i only active on <a href="https://discord.gg/memes" target="__blank">discord</a> :)
-- just so you know, 2025 is 5.23% complete- just so you know, 2025 is 10.15% complete
-- just so you know, 2025 is 10.42% complete
-- just so you know, 2025 is 10.70% complete
-- just so you know, 2025 is 10.97% complete
--- index.rs
+++ index.rs
@@ -5,40 +5,50 @@
 /* but anyway, this is my implementation: */
 
 use std::fs;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{ SystemTime, UNIX_EPOCH };
 
 fn main() {
-  let current_time = SystemTime::now();
+    let current_time = SystemTime::now();
 
-  let current_year = 1970 + current_time
-    .duration_since(UNIX_EPOCH)
-    .expect("time went backwards")
-    .as_secs() / 31_536_000;
+    let current_year =
+        1970 +
+        current_time.duration_since(UNIX_EPOCH).expect("time went backwards").as_secs() /
+            31_536_000;
 
-  let start_of_year = UNIX_EPOCH
-    .checked_add(std::time::Duration::from_secs(
-      (current_year as u64 - 1970) * 31_536_000,
-    ))
-    .expect("invalid date");
+    let start_of_year = UNIX_EPOCH.checked_add(
+        std::time::Duration::from_secs(((current_year as u64) - 1970) * 31_536_000)
+    ).expect("invalid date");
 
-  let is_leap_year = (current_year % 4 == 0 && current_year % 100 != 0) || (current_year % 400 == 0);
-  let seconds_in_year = if is_leap_year { 366 * 24 * 60 * 60 } else { 365 * 24 * 60 * 60 };
+    let is_leap_year =
+        (current_year % 4 == 0 && current_year % 100 != 0) || current_year % 400 == 0;
+    let seconds_in_year = if is_leap_year { 366 * 24 * 60 * 60 } else { 365 * 24 * 60 * 60 };
 
-  let progress_of_this_year = current_time
-    .duration_since(start_of_year)
-    .expect("time calculation error")
-    .as_secs() as f64
-    / seconds_in_year as f64;
+    let progress_of_this_year =
+        (
+            current_time
+                .duration_since(start_of_year)
+                .expect("time calculation error")
+                .as_secs() as f64
+        ) / (seconds_in_year as f64);
 
-  let readme_path = "README.md";
-  let mut readme_content = fs::read_to_string(readme_path).expect("unable to read README.md");
+    let readme_path = "README.md";
+    let readme_content = fs::read_to_string(readme_path).expect("unable to read README.md");
 
-  let progress_info = format!(
-    "- just so you know, {} is {:.2}% complete\n",
-    current_year,
-    progress_of_this_year * 100.0
-  );
-  readme_content.push_str(&progress_info);
+    let mut updated_content = String::new();
+    for line in readme_content.lines() {
+        if line.starts_with("- just so you know,") {
+            let updated_line = format!(
+                "- just so you know, {} is {:.2}% complete",
+                current_year,
+                progress_of_this_year * 100.0
+            );
+            updated_content.push_str(&updated_line);
+            updated_content.push('\n');
+        } else {
+            updated_content.push_str(line);
+            updated_content.push('\n');
+        }
+    }
 
-  fs::write(readme_path, readme_content).expect("Unable to write to README.md");
+    fs::write(readme_path, updated_content).expect("Unable to write to README.md");
 }
